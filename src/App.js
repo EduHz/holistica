b07f9654e7d2a2056b5bb4a6216d4d84810afb59
EduHz <eduhzprogramador@gmail.com>
2023-05-22 19:52:45 -0300

    changues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/NavBar";
 
 export default function App() {
   return (
     <div className="app">
+      <Navbar />
       <h1>Soy App</h1>
     </div>
   );
