de7bffcaf33f20e28c640592b9adb4ae26bf4a75
EduHz <eduhzprogramador@gmail.com>
2023-05-28 13:13:19 -0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,13 +5,12 @@ import wallpaper from "../images/wallpaperflare.com_wallpaper.jpg";
 import "./Hero.css";
 
 const Hero = () => (
-  <div className="text-center hero my-5">
+  <div
+    className="text-center hero my-5"
+    style={{ backgroundImage: `url(${wallpaper})` }}
+  >
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
     <h1 className="mb-4">React.js Sample Project</h1>
-
-    <div className="image-container">
-      <img className="background-image" src={wallpaper} alt="Background" />
-    </div>
 
     <p className="lead">
       This is a sample application that demonstrates an authentication flow for
